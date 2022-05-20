ae64b8ce91345313eb29d56702e7d8ab8f5d5ebb
soapmans <soapmans@icloud.com>
2022-05-20 21:06:46 +0800

    delete

--- src/main/java/com/soapmans/changya/service/HeathCheckInService.java
+++
@@ -1,9 +0,0 @@
-package com.soapmans.changya.service;
-
-import com.mashape.unirest.http.exceptions.UnirestException;
-
-public interface HeathCheckInService {
-
-     String heathCheckIn() throws UnirestException;
-
-}
